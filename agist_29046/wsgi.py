2dedde9bd499d62451b75e840cf2effe5f76a77a
Crowdbotics <team@crowdbotics.com>
2021-07-23 01:22:14 +0000

    Update models

--- agist_29046/wsgi.py
+++ agist_29046/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agist_29046.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agist_29046.settings")
 
 application = get_wsgi_application()
